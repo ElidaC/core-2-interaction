acde5b667c251e7f8cb7335e51c7363530c3152e
ElidaC <chens091@newschool.edu>
2024-03-06 18:05:01 -0500

    all

---
+++ lab/projects/04_collection-object/assets/data.js
@@ -0,0 +1,13 @@
+console.log('\n\nARRAY OF OBJECTS\n\n\n');
+
+var data = [
+    {
+        buildingName: 'Mannes',
+        vibe: 'stylisyic',
+        material: '',
+        size: 'middle',
+        major: 'fashion design',
+        time: 'Feb 28, 2024 - 8:39AM',
+    },
+   
+];
