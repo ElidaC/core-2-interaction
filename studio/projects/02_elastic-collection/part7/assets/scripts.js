35b62fddef365fcb8dcd64365b71ee078e31214c
ElidaC <chens091@newschool.edu>
2024-03-25 00:36:11 -0400

    push

--- studio/projects/02_elastic-collection/part7/assets/scripts.js
+++ studio/projects/02_elastic-collection/part7/assets/scripts.js
@@ -120,7 +120,7 @@ function drop(ev) {
     var draggedItem = document.getElementById(data);
     var tote = document.querySelector('.Tote');
 
-    if (data === 'item1' || data === 'item2' || data === 'item3') {
+    if (data === 'item1' || data === 'item2' || data === 'item3' || data === 'item4' || data === 'item5' || data === 'item6') {
         tote.appendChild(draggedItem);
         draggedItem.style.position = 'absolute';
         draggedItem.style.top = (ev.clientY - tote.getBoundingClientRect().top - draggedItem.clientHeight / 2) + 'px';
